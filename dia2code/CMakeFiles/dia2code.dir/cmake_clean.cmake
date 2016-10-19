ce32f0b668cf70556b2b9bbaf8abd0b024523e0f
alex <alexis.loison@ensea.fr>
2016-10-19 16:55:31 +0200

    a

---
+++ dia2code/CMakeFiles/dia2code.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dia2code.dir/src/generate_code_ada.c.o"
+  "CMakeFiles/dia2code.dir/src/generate_code_cpp.c.o"
+  "CMakeFiles/dia2code.dir/src/generate_code_shp.c.o"
+  "CMakeFiles/dia2code.dir/src/main.c.o"
+  "CMakeFiles/dia2code.dir/src/dia2code.c.o"
+  "CMakeFiles/dia2code.dir/src/source_parser.c.o"
+  "CMakeFiles/dia2code.dir/src/generate_code_ruby.c.o"
+  "CMakeFiles/dia2code.dir/src/generate_code_idl.c.o"
+  "CMakeFiles/dia2code.dir/src/comment_helper.c.o"
+  "CMakeFiles/dia2code.dir/src/generate_code_python.c.o"
+  "CMakeFiles/dia2code.dir/src/generate_code_sql.c.o"
+  "CMakeFiles/dia2code.dir/src/generate_code_php.c.o"
+  "CMakeFiles/dia2code.dir/src/generate_code_java.c.o"
+  "CMakeFiles/dia2code.dir/src/decls.c.o"
+  "CMakeFiles/dia2code.dir/src/generate_code_as3.c.o"
+  "CMakeFiles/dia2code.dir/src/includes.c.o"
+  "CMakeFiles/dia2code.dir/src/generate_code_c.c.o"
+  "CMakeFiles/dia2code.dir/src/generate_code_csharp.c.o"
+  "CMakeFiles/dia2code.dir/src/scan_tree.c.o"
+  "CMakeFiles/dia2code.dir/src/parse_diagram.c.o"
+  "CMakeFiles/dia2code.dir/src/generate_code_php_five.c.o"
+  "bin/dia2code.pdb"
+  "bin/dia2code"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dia2code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
